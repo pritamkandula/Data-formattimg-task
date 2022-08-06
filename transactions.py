c7903fb688e4aa839af0923f2b71b1df418f20af
pritamkandula <67675851+pritamkandula@users.noreply.github.com>
2022-08-06 22:33:49 +0530

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -3,3 +3,13 @@ from overview import user_data
 trans = user_data('SampleData.json')
 for i in range(len(trans.index)):
      print('The User_id {} has made {} transactions in the 10 months period.'.format(trans.iloc[i].name,trans.iloc[i].sum()))
+
+with open("Sampledata.json",'r') as f:
+    saat = json.load(f)
+  
+merchants = []
+for i in saat:
+    if (saat[i]['user_id']) == (saat[i]['k_user_id']):
+        merchants.append(saat[i]['user_id'])
+        
+print('\nUser_ids who utilized the Merchant or K_user service in the 10 months period are:\n',merchants)

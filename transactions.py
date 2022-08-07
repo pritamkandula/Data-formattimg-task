0e61cf8ea83fe20dfdebb387e63f3bd34b322331
pritamkandula <67675851+pritamkandula@users.noreply.github.com>
2022-08-07 11:23:35 +0530

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,5 +1,6 @@
 from overview import user_data
 
+#calling the function to get the dataframe from overview.py and performing the required operations
 trans = user_data('SampleData.json')
 for i in range(len(trans.index)):
      print('The User_id {} has made {} transactions in the 10 months period.'.format(trans.iloc[i].name,trans.iloc[i].sum()))

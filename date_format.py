4e75e325969a74c8b67ff4513e2afb1062016233
pritamkandula <67675851+pritamkandula@users.noreply.github.com>
2022-08-07 11:06:47 +0530

    Update date_format.py

--- date_format.py
+++ date_format.py
@@ -3,27 +3,30 @@ import datetime as dt
 
 
 def format_dates(json_file):
+        #loading the json file
         with open(json_file,'r') as f:
             saat = json.load(f)
 
         epoch_time = []
-
+        #extracting all the epoch timestamps and storing it as a list in epoch_time empty list
         for ep in saat:
             epoch = saat[ep]['redeemed_at']
             epoch_time.append(epoch)
 
-
+        #converting all the timestamps in the list to seconds
         for i in range(len(epoch_time)):
             epoch_time[i] = epoch_time[i]/1000
         
         proper = []
+        #performing the conversion of the epoch timestamps to a proper date format using the datetime module
         for i in range(len(epoch_time)):
             extracted_date = dt.datetime.fromtimestamp(epoch_time[i])
             proper_date = extracted_date.strftime("%d-%b-%Y")
             proper.append(proper_date)
 
-        
+        #output
         print('List of dates in proper format:\n', proper )
+        #checking if the number of values before conversion and after conversion are equal
         print('Total values in epoch_time:', len(epoch_time))
         print('Total values in proper:', len(proper))
 
